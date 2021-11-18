aa8a7bf30660254d0b999564a2e5d56280c58a07
KHenaghen <khenaghen@gmail.com>
2021-11-17 16:17:40 -0800

    Build

---
+++ build/precache-manifest.0aaa41176705885d60ca895cfe10939f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aad5f74d1f1445dde19ff25a78e8722",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8adb3a1cab9e6012fcc7",
+    "url": "/static/js/2.6c99d600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c99d600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da26996494d2c61b00b",
+    "url": "/static/js/main.01f8cbfa.chunk.js"
+  },
+  {
+    "revision": "aa7c612222590812d412",
+    "url": "/static/js/runtime-main.1160daa0.js"
+  }
+]);
